6b183c162ed8f4779c6c55bfecc8bbe3e1d68db5
mkaesz <mkaesz@users.noreply.github.com>
2021-08-23 13:57:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 resource "null_resource" "hello_world" {
   provisioner "local-exec" {
+    command = "env"
+  }
+}
+
+resource "null_resource" "hello_world2" {
+  provisioner "local-exec" {
     command = "echo blub"
   }
 }
+
