bccf237da82875b37981047024d3bc6871271138
mkaesz <mkaesz@users.noreply.github.com>
2022-08-02 10:10:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,9 @@ resource "null_resource" "hello_world2" {
     command = "env; echo blub"
   }
 }
+
+resource "null_resource" "hello_world2" {
+  provisioner "local-exec" {
+    command = "env; echo blub"
+  }
+}
